ca9390f2aa0c17beca542c91047fb8ef6717cc06
Andrei <andrei@MacBook-Air-Andrei.local>
2023-05-08 13:18:39 +0300

    :refactor:delete trim

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,21 +4,20 @@ const {stat} = require('fs')
 
 
 
-async function readdir(){
-try {
-  const files = await fs.readdir(path.join(__dirname,'secret-folder'),{encoding:'utf-8', withFileTypes:true});
-  for (const file of files)
-    if(!file.isDirectory()){
+async function readdir() {
+  try {
+    const files = await fs.readdir(path.join(__dirname,'secret-folder'),{encoding:'utf-8', withFileTypes:true});
+    for (const file of files)
+      if(!file.isDirectory()) {
         const ext = path.extname(file.name)
         const baseName = path.basename(file.name,ext)
-       stat(path.join(__dirname,`secret-folder/${file.name}`),(err,stats)=>{
-        if(err) throw err  
-        let fileSize = stats.size + 'kb'
-        console.log(`${baseName} - ${ext.slice(1)} - ${fileSize} `)
+        stat(path.join(__dirname,`secret-folder/${file.name}`),(err,stats)=>{
+          if(err) throw err  
+          let fileSize = stats.size + 'kb'
+          console.log(`${baseName} - ${ext.slice(1)} - ${fileSize} `)
         })
-        
       }
-} catch (err) {
+  } catch (err) {
   console.error(err);
 }
 }
