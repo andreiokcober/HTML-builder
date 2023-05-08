cbecc606a72fcfbf56b8daac1830a1164312c994
Andrei <andrei@MacBook-Air-Andrei.local>
2023-05-08 16:08:44 +0300

    :refactor js code 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,9 +1,10 @@
 const fs = require('fs')
 const path = require('path')
+const styles = path.join(__dirname,'styles')
 const bundle = fs.createWriteStream(path.join(__dirname,'project-dist','bundle.css'))
 
-const createCssFile = (way)=>{
-    fs.readdir(way,{withFileTypes: true},(err,files)=>{
+const createCssFile = (direction)=>{
+    fs.readdir(direction,{withFileTypes: true},(err,files)=>{
         if(err) throw err
        files.forEach((file)=>{
            if(file.isFile() && path.extname(file.name) === '.css'){
@@ -11,40 +12,43 @@ const createCssFile = (way)=>{
            }
        })
     })
-}
-createCssFile(path.join(__dirname,'styles'))
-
-
+}  
+createCssFile(styles) 
 function copyCssFile(file){
     const rs = fs.createReadStream(path.join(__dirname,'styles',file),'utf-8')
     rs.pipe(bundle)
     rs.on('error', err=> console.log(err))
-
-
 }
+
+
+
+
+                                        /* 2-й вариант решения не доделанный*/
 // async function createCssStyles(){
-//     const readStyles = await fss.readdir(path.join(__dirname, 'styles'),{encoding:'utf-8', withFileTypes:true} )
-//     const arrCssFile = []   
-//     for(file of readStyles){
-//         if(!file.isDirectory() && path.extname(file.name) === '.css'){
-//             arrCssFile.push(file.name)
+//     fs.writeFile(
+//         path.join(__dirname,'project-dist','bundle.css'),
+//         '',
+//         ( err ) => {
+//             if(err) throw err
+//         }
+//     ) 
+//     fs.readdir(path.join(__dirname, 'styles'),{encoding:'utf-8', withFileTypes:true},(err,data) => {
+//         if(err) throw err  
+//     for(let file of data){
+//         if(!file.isDirectory() && path.extname(file.name) === '.css') {
+//             const rs = fs.createReadStream(path.join(__dirname,'styles',file.name,(err, data) => {
+//                 console.log(data)
+//             }))
+//             fs.appendFile(
+//                 path.join(__dirname,'project-dist','bundle.css'),
+//                 `${rs}`,
+//                 (err) => {
+//                 if(err) throw err
+//             })
+            
+        
 //         }
 //     }
-//     console.log(arrCssFile)
-//     const creatDir = await fs.writeFile(path.join(__dirname,'project-dist','bundle.css'),'',(err,data)=>{
-//         if(err) throw err
-        
 //     })
-//     arrCssFile.forEach(async (file)=>{
-     
-//          fs.copyFile(path.join(__dirname,'styles',file),path.join(__dirname,'project-dist','bundle.css'),(err)=>{
-//             if(err) throw err
-            
-//         })  
-//     })
-    
-    
-        
-    
 // }
 // createCssStyles()
