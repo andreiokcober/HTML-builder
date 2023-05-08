25a78e0444bdc1c43451856f60f1678e6c76d6ce
Andrei <andrei@MacBook-Air-Andrei.local>
2023-05-08 14:17:51 +0300

    :refactor: change 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,23 +1,23 @@
 const fs = require('fs/promises')
 const path = require('path')
+const { json } = require('stream/consumers')
 const newFile = path.join(__dirname,'files-copy')
 const oldFile = path.join(__dirname, 'files')
 
- async function copyDir(){
-      fs.rm(path.join(__dirname,'files-copy'),{recursive:true,force:true})
-      .then(()=>{
-           fs.mkdir(newFile,{recursive:true})
-             fs.readdir(oldFile,{withFileTypes: true})
-             .then((files)=>{
-                 files.forEach((file)=>{
-                     if(file.isFile()){
-                         let firstFilePath = path.join(oldFile,file.name)
-                         let secondFilePath = path.join(newFile,file.name)
-                         fs.copyFile(firstFilePath,secondFilePath)
-                     }
-                 })
-             })    
-          
-      })        
-     } 
+async function copyDir(){
+    fs.rm(path.join(__dirname,'files-copy'),{recursive:true,force:true})
+    .then(()=>{
+        fs.mkdir(newFile,{recursive:true})
+        fs.readdir(oldFile,{withFileTypes: true})
+        .then((files)=>{
+            files.forEach((file)=>{
+                if(file.isFile()){
+                    let firstFilePath = path.join(oldFile,file.name)
+                    let secondFilePath = path.join(newFile,file.name)
+                    fs.copyFile(firstFilePath,secondFilePath)
+                }
+            })
+        })     
+    })        
+} 
 copyDir()
